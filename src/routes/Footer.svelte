c9e4875d3184add3e989d0a55fbdcb08dc864222
MrAdhit <adhityarimba97@gmail.com>
2023-04-23 12:31:09 +0700

    Redesign footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -6,24 +6,16 @@
     <div class="wrapper">
         <div class="copyright">
             <img src="https://anonupload.tech/files/1681603239958_lorestone.png/view" alt="">
-            <span>Copyright © 2023 Lorestone.</span>
-            <span>All rights reserved</span>
+            <span>Lorestone. © 2023</span>
             <div class="icon">
                 <a class="discord" target=”_blank” href="https://discord.gg/lorestone"><Icon icon="fa6-brands:discord"/></a>
             </div>
         </div>
         <div class="links">
-            <span>Quick Links</span>
-            <div class="link">
-                <a href="/store" class="link-item">Store</a>
-                <a href="/staff" class="link-item">Staff</a>
-            </div>
-        </div>
-        <div class="support">
-            <span>Help & Support</span>
-            <div class="link">
-                <a href="https://discord.gg/lorestone" target=”_blank” class="link-item">Discord Server</a>
-            </div>
+            <a href="/store" class="link">Store</a>
+            <a href="/staff" class="link">Staff</a>
+            <a href="/rules" class="link">Rules</a>
+            <a href="https://discord.gg/lorestone" target=”_blank” class="link">Discord Server</a>
         </div>
     </div>
 </footer>
@@ -36,11 +28,14 @@
     }
 
     footer {
-        height: max-content;
+        height: min-content;
         background-color: $brown-cold;
 
-        padding-top: 8vh;
-        padding-bottom: 8vh;
+        padding-top: 40px;
+        padding-bottom: 40px;
+
+        font-size: 13px;
+        color: rgba(#FFF, $alpha: 0.5);
 
         display: flex;
         justify-content: center;
@@ -48,10 +43,11 @@
 
         .wrapper {
             display: flex;
-            justify-content: space-around;
-            align-items: start;
+            justify-content: space-between;
+            align-items: center;
 
-            width: 70vw;
+            height: fit-content;
+            width: 65%;
 
             @media screen and (max-width: 768px) {
                 flex-direction: column;
@@ -59,27 +55,27 @@
                 align-items: center;
             }
 
-            > .support, .links {
+            > .links {
                 display: flex;
                 flex-direction: column;
+                height: 120px;
+                flex-wrap: wrap;
+                flex: 1;
 
-                > .link {
-                    display: flex;
-                    flex-direction: column;
+                font-size: 14px;
 
-                    @media screen and (max-width: 768px) {
-                        align-items: center;
-                        margin-bottom: 30px;
-                    }
-
-                    > .link-item {
-                        margin-bottom: 5px;
-                    }
+                @media screen and (max-width: 768px) {
+                    margin-top: 20px;
+                    align-items: center;
                 }
 
-                > span {
-                    margin-bottom: 20px;
-                    font-weight: 700;
+                > .link {
+                    padding: 5px;
+                    width: fit-content;
+
+                    &:hover {
+                        color: rgba(#FFF, $alpha: 1);
+                    }
                 }
             }
 
@@ -87,34 +83,33 @@
                 display: flex;
                 flex-direction: column;
                 align-items: start;
+                padding-right: 50px;
 
                 > img {
-                    width: 200px;
-                    margin-bottom: 20px;
+                    width: 150px;
                 }
 
                 > span {
-                    margin-top: 5px;
-                    margin-bottom: 5px;
+                    margin-top: 10px;
                 }
 
                 > .icon {
-                    margin-top: 20px;
-
+                    margin-top: 5px;
                     > .discord {
                         aspect-ratio: 1;
-                        font-size: 24px;
+                        font-size: 16px;
+
+                        &:hover {
+                            color: #7289DA;
+                        }
                     }
                 }
 
                 @media screen and (max-width: 768px) {
-                    order: 100;
                     align-items: center;
-                    margin-top: 50px;
+                    padding-right: 0;
                 }
             }
-        }
-
-        
+        }        
     }
 </style>
